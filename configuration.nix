a6b8afa4becbd1bfdde00c8e7d65135c43c5a779
ptdewey <patrickdewey2002@gmail.com>
2024-09-25 19:02:38 -0400

    initial commit

---
+++ configuration.nix
@@ -0,0 +1,173 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  nix.nixPath = [ "nixos-config=/home/patrick/configuration.nix" ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "callisto";
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  programs.hyprland.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.patrick = {
+    isNormalUser = true;
+    description = "Patrick Dewey";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    git
+    nemo
+    wofi
+    curl
+    cacert
+    firefox-unwrapped
+    firefox-devedition-unwrapped
+    google-chrome
+    kitty
+    gcc
+    fd
+    feh
+    htop
+    killall
+    lm_sensors
+    tmux
+    unzip
+    tree
+    wezterm
+    acpi
+    fzf
+    go
+    ripgrep
+    brightnessctl
+    pulseaudio
+    copyq
+    waybar
+    swaybg
+    scowl
+    discord
+    zsh
+    wl-clipboard
+    xclip
+    networkmanagerapplet
+    gnumake
+    bluez
+    swayfx
+  ];
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+
+  programs.zsh = {
+    enable = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+  };
+
+  services.power-profiles-daemon.enable = true;
+
+  users.defaultUserShell = pkgs.zsh;
+  users.users.patrick.shell = pkgs.zsh;
+
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [
+      # add missing dynamic libs here
+      sqlite
+    ];
+  };
+
+  environment.variables = {
+    LD_LIBRARY_PATH = "${pkgs.sqlite}/lib:${builtins.getEnv "LD_LIBRARY_PATH"}";
+  };
+
+  environment.wordlist = {
+    enable = true;
+    lists = {
+      WORDLIST = [ "${pkgs.scowl}/share/dict/words.txt" ];
+    };
+  };
+
+  services.gvfs.enable = true;
+  services.udisks2.enable = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Patricks NixOS System Flake";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations = let
+      # define common modules shared between systems
+      commonModules = [
+        # can also include module files here
+        # ./modules/r-env.nix
+
+        # ({ pkgs, ... }: {
+        #     nixpkgs.overlays = [ neovim-nightly-overlay.overlay ];
+        # })
+      ];
+    in {
+      # define different systems
+      callisto = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = commonModules ++ [
+          ./configuration.nix
+          # ./hosts/callisto/hardware-extras.nix
+          # ./hosts/callisto/default.nix
+          # ./modules/default.nix
+        ];
+      };
+    };
+  };
+}
