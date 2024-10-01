450593a41c0840da212347acdc86b61c404a123a
ptdewey <noreply>
2024-10-01 15:05:59 -0400

    NixOS build #39

--- configuration.nix
+++ configuration.nix
@@ -140,6 +140,12 @@
   services.gvfs.enable = true;
   services.udisks2.enable = true;
 
+
+  # bluetooth things
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
