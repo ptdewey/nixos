5c99a77641ba131ad88ba51397dc8f6d939b4287
ptdewey <noreply>
2024-10-05 11:17:55 -0400

    NixOS build #41

--- configuration.nix
+++ configuration.nix
@@ -152,6 +152,7 @@
     setSocketVariable = true;
   };
 
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -170,6 +171,10 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
+
+  networking.extraHosts = ''
+    192.168.68.106  kelsier
+  '';
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
