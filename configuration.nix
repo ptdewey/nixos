0ada7b9384499ef022a5454aa451d589c803edc9
ptdewey <noreply>
2024-10-09 16:29:45 -0400

    NixOS build #46

--- configuration.nix
+++ configuration.nix
@@ -6,7 +6,7 @@
       ./hardware-configuration.nix
     ];
 
-  nix.nixPath = [ "nixos-config=/home/patrick/configuration.nix" ];
+  nix.nixPath = [ "nixos-config=/home/patrick/configuration.nix" "nixpkgs=channel:nixos-unstable"];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
