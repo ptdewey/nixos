af9e1043af475e43ef8cffd795fe0b5eda95db8e
ptdewey <noreply>
2024-10-09 16:09:49 -0400

    NixOS build #44

--- configuration.nix
+++ configuration.nix
@@ -124,7 +124,7 @@
 
   # add missing dynamic libs (do not include in environment.systemPackages)
   programs.nix-ld.enable = true;
-  programs.nix-ld.libraries = with pkgs; [ sqlite ];
+  programs.nix-ld.libraries = with pkgs; [ sqlite luajitPackages.sqlite ];
 
   environment.wordlist = {
     enable = true;
