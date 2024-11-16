b6b6b8d072599edf4c3a91519b1eb53796130ba2
ptdewey <noreply>
2024-11-16 13:13:12 -0500

    NixOS build #78

--- modules/sddm-theme.nix
+++ modules/sddm-theme.nix
@@ -1,5 +1,5 @@
 # sddm theme
-{ config, lib, pkgs, ... }:
+{ pkgs, ... }:
 
 let
     sddmTheme = pkgs.stdenv.mkDerivation {
@@ -9,10 +9,6 @@ let
                 repo = "sddm-astronaut-theme";
                 rev = "468a100460d5feaa701c2215c737b55789cba0fc";
                 sha256 = "1h20b7n6a4pbqnrj22y8v5gc01zxs58lck3bipmgkpyp52ip3vig";
-                # owner = "MarianArlt";
-                # repo = "sddm-sugar-dark";
-                # rev = "ceb2c455663429be03ba62d9f898c571650ef7fe";
-                # sha256 = "0153z1kylbhc9d12nxy9vpn0spxgrhgy36wy37pk6ysq7akaqlvy";
         };
         installPhase = ''
             mkdir -p $out
