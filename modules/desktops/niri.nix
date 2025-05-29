c7babc2965679293c1707253ff0b9cb72c35112e
ptdewey <noreply>
2025-05-29 10:57:18 -0400

    europa: NixOS build #70

--- modules/desktops/niri.nix
+++ modules/desktops/niri.nix
@@ -9,4 +9,16 @@
   ];
 
   programs.niri.enable = true;
+
+  systemd.user.services = {
+    swaybg = {
+      description = "Wallpaper Service";
+      after = [ "niri.service" ];
+      wantedBy = [ "graphical-session.target" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.swaybg}/bin/swaybg -m fill -i %h/Pictures/wallpapers/evergarden.png";
+        Restart = "on-failure";
+      };
+    };
+  };
 }
