b62afa7086cf12f207ed7ed1a7746180fc9ab14d
ptdewey <noreply>
2025-06-15 12:14:00 -0400

    europa: NixOS build #110

--- modules/common.nix
+++ modules/common.nix
@@ -57,6 +57,7 @@
     bibata-cursors
     typst
     # opencode # TODO: install once it updates to the 1.49+ sst version (can't get 0.49 to work)
+    bun
   ];
 
   # Add missing dynamic libs (do not include in environment.systemPackages)
