4fb227e983c94972fbcd3eda99e139a94f1c49aa
ptdewey <noreply>
2026-03-14 21:17:19 -0400

    europa: NixOS build #220

--- modules/apps/discord.nix
+++ modules/apps/discord.nix
@@ -3,7 +3,8 @@ let
   discord-sandboxed = pkgs.buildFHSEnv {
     name = "discord";
     targetPkgs = pkgs: [ pkgs.discord ];
-    runScript = "discord";
+    runScript =
+      "discord --enable-features=UseOzonePlatform --ozone-platform=wayland";
     extraBwrapArgs = [ "--unshare-pid" "--unshare-uts" ];
   };
 in { environment.systemPackages = [ discord-sandboxed ]; }
