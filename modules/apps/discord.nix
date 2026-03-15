308c4948a6a9f13573e885a4aab7e40c6837a62e
ptdewey <noreply>
2026-03-14 21:09:23 -0400

    europa: NixOS build #219

--- modules/apps/discord.nix
+++ modules/apps/discord.nix
@@ -1,18 +1,9 @@
 { pkgs, ... }:
 let
-  discord-sandboxed = pkgs.writeShellScriptBin "discord" ''
-    exec ${pkgs.bubblewrap}/bin/bwrap \
-      --ro-bind /nix /nix \
-      --ro-bind /run/current-system /run/current-system \
-      --ro-bind /etc /etc \
-      --bind /tmp /tmp \
-      --bind "$XDG_RUNTIME_DIR" "$XDG_RUNTIME_DIR" \
-      --bind "$HOME/.config/discord" "$HOME/.config/discord" \
-      --bind "$HOME/Downloads" "$HOME/Downloads" \
-      --dev /dev \
-      --proc /proc \          # you can --ro-bind or restrict further
-      --unshare-pid \         # hides other PIDs
-      --unshare-uts \
-      -- ${pkgs.discord}/bin/discord "$@"
-  '';
+  discord-sandboxed = pkgs.buildFHSEnv {
+    name = "discord";
+    targetPkgs = pkgs: [ pkgs.discord ];
+    runScript = "discord";
+    extraBwrapArgs = [ "--unshare-pid" "--unshare-uts" ];
+  };
 in { environment.systemPackages = [ discord-sandboxed ]; }
