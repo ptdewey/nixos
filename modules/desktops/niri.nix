c3655e19eeb8bcbce579e7f11f48b29fd568f2df
ptdewey <noreply>
2026-09-02 20:21:03 -0400

    europa: NixOS build #265

--- modules/desktops/niri.nix
+++ modules/desktops/niri.nix
@@ -18,7 +18,7 @@
       after = [ "niri.service" ];
       wantedBy = [ "graphical-session.target" ];
       serviceConfig = {
-        ExecStart = "${pkgs.swaybg}/bin/swaybg -m fill -i %h/Pictures/wallpapers/evergarden.png";
+        ExecStart = "${pkgs.swaybg}/bin/swaybg -m fill -i %h/Pictures/wallpapers/ghibli-city.png";
         Restart = "on-failure";
       };
     };
